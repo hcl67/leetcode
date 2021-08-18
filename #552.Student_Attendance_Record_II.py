7dff334da94c054492e8e64696554c3d7fcf351a
hcl67 <djinn.luu@gmail.com>
2021-08-18 15:27:23 +0100

    Update #552.Student_Attendance_Record_II.py

--- #552.Student_Attendance_Record_II.py
+++ #552.Student_Attendance_Record_II.py
@@ -1,6 +1,10 @@
 '''
 https://leetcode.com/problems/student-attendance-record-ii/
 '''
+'''
+1.考虑没有A的情况，三种情况 P, PL, PLL, 有 P(n+1) = P(n)+PL(n)+PLL(n)；PL(n+1)=P(n)；PLL(n+1)=PL(N)
+2.考虑加入一个A，是没有A的两侧的乘积
+'''
 class Solution:
     def checkRecord(self, n: int) -> int:
         K = 10**9+7
