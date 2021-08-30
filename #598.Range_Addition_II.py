f0f911dadbc0bb530e7cb601b8ea4a36a9b6f004
hcl67 <djinn.luu@gmail.com>
2021-08-30 15:16:22 +0100

    Update #598.Range_Addition_II.py

--- #598.Range_Addition_II.py
+++ #598.Range_Addition_II.py
@@ -1,11 +1,12 @@
 '''
 https://leetcode.com/problems/range-addition-ii/
 '''
-
 class Solution:
     def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
-        a,b = m,n
-        for op in ops:
-            a,b = min(op[0],a),min(op[1],b)
+        a = m
+        b = n
+        for i in range(len(ops)):
+            a = min(ops[i][0],a)
+            b = min(ops[i][1],b) 
         return a*b
         
