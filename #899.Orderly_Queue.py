ecc974799272cbeebf8aa9423287aad4de752ffc
hcl67 <djinn.luu@gmail.com>
2021-09-05 11:46:24 +0100

    Update #899.Orderly_Queue.py

--- #899.Orderly_Queue.py
+++ #899.Orderly_Queue.py
@@ -5,15 +5,11 @@ class Solution:
     def orderlyQueue(self, s: str, k: int) -> str:
         #当k>=2时，可以视作冒泡排序
         if k == 1:
-            c = min(list(s))
-            i = s.find(c)
-            l = [c+s[i+1:]+s[:i]]
-            while 1:
-                i = s.find(c,i+1)
-                if i < 0:
-                    break
-                l += [c+s[i+1:]+s[:i]]
-            return min(l)            
+            l = []
+            for i in range(len(s)):
+                s = s[1:]+s[:1]
+                l += [s]
+            return min(l)        
         else:
-            return ''.join(sorted(s))
+            return ''.join(sorted(s))     
         
