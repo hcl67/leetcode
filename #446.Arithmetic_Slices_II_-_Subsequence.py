5e0b821ca20057751e220e0070ad141bb9513041
hcl67 <djinn.luu@gmail.com>
2021-09-10 19:53:30 +0100

    Create #446.Arithmetic_Slices_II_-_Subsequence.py

---
+++ #446.Arithmetic_Slices_II_-_Subsequence.py
@@ -0,0 +1,17 @@
+'''
+https://leetcode.com/problems/arithmetic-slices-ii-subsequence/
+'''
+#未做出，下位官方答案
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        ans = 0
+        f = [defaultdict(int) for _ in nums]
+        for i, x in enumerate(nums):
+            for j in range(i):
+                d = x - nums[j]
+                cnt = f[j][d]
+                ans += cnt
+                f[i][d] += cnt + 1
+        return ans
+
+        
