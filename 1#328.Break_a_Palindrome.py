ec954dac23a52b74a327efdeed74c5263b9ca77e
hcl67 <djinn.luu@gmail.com>
2021-09-23 11:24:24 +0100

    Create 1#328.Break_a_Palindrome.py

---
+++ 1#328.Break_a_Palindrome.py
@@ -0,0 +1,15 @@
+'''
+https://leetcode.com/problems/break-a-palindrome/
+'''
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        if len(palindrome) <= 1:
+            return ''
+        i = 0
+        while i < len(palindrome)//2:
+            if palindrome[i] != 'a':
+                return palindrome[:i] + 'a' + palindrome[i+1:]
+            i += 1
+        return palindrome[:-1] + 'b'
+                    
+            
