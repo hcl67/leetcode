373777dda837d6c6fe3d183ce164115b62b64c70
hcl67 <djinn.luu@gmail.com>
2021-10-21 11:49:15 +0100

    Update #380.Insert_Delete_GetRandom_O(1).py

--- #380.Insert_Delete_GetRandom_O(1).py
+++ #380.Insert_Delete_GetRandom_O(1).py
@@ -1,6 +1,44 @@
 '''
 https://leetcode.com/problems/insert-delete-getrandom-o1/
 '''
+# 看了答案提示后的代码
+class RandomizedSet:
+    from random import choice
+
+    def __init__(self):
+        self.d = {}
+        self.l = []
+        
+
+    def insert(self, val: int) -> bool:
+        if val in self.d:
+            return False
+        else:
+            self.d[val] = len(self.d)
+            self.l += [val]
+            return True
+        
+
+    def remove(self, val: int) -> bool:
+        if val in self.s:
+            i = self.d[val]
+            v = list[-1]
+            l[i],l[-1] = l[-1],l[i]
+            self.d[v] = i
+            del self.d[val]
+            self.l.pop()
+            return True
+        else:
+            return False
+        
+
+    def getRandom(self) -> int:
+        return choice(self.l)
+        
+
+
+# 其实不满足题目要求
+'''
 class RandomizedSet:
     from random import choice
 
@@ -27,7 +65,7 @@ class RandomizedSet:
     def getRandom(self) -> int:
         return choice(list(self.s))
         
-        
+ '''       
 
 
 # Your RandomizedSet object will be instantiated and called as such:
