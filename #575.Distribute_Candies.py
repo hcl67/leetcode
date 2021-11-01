46e6bacbf803a7f381b963d356ca6368e31de2f6
hcl67 <djinn.luu@gmail.com>
2021-11-01 12:22:15 +0000

    Create #575.Distribute_Candies.py

---
+++ #575.Distribute_Candies.py
@@ -0,0 +1,6 @@
+'''
+https://leetcode.com/problems/distribute-candies/
+'''
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        return min(len(set(candyType)),len(candyType)//2)
