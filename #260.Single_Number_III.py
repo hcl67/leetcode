08e2b81a9f322e5015a50ba8f6ae2fb2664d2782
hcl67 <djinn.luu@gmail.com>
2021-11-06 12:38:36 +0000

    Update #260.Single_Number_III.py

--- #260.Single_Number_III.py
+++ #260.Single_Number_III.py
@@ -3,6 +3,7 @@ https://leetcode.com/problems/single-number-iii/
 '''
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
+        '''
         d = {}
         for i in nums:
             if i in d:
@@ -10,4 +11,18 @@ class Solution:
             else:
                 d[i] = 1
         return list(d.keys())
-        
+        '''
+        #看答案提示后
+        x = 0
+        for n in nums:
+            x ^= n
+        f = 1
+        while x&f == 0:
+            f<<=1
+        x,y = 0,0
+        for n in nums:
+            if n&f:
+                x ^= n
+            else:
+                y ^= n
+        return [x,y]
