20188f825c1ee360e8a86c173b00e8b0cda69bbf
hcl67 <djinn.luu@gmail.com>
2021-11-16 13:46:15 +0000

    Update #668.Kth_Smallest_Number_in_Multiplication_Table.py

--- #668.Kth_Smallest_Number_in_Multiplication_Table.py
+++ #668.Kth_Smallest_Number_in_Multiplication_Table.py
@@ -1,6 +1,8 @@
 '''
 https://leetcode.com/problems/kth-smallest-number-in-multiplication-table/
 '''
+
+'''
 class Solution:
     def findKthNumber(self, m: int, n: int, k: int) -> int:
         def _c(x):
@@ -17,7 +19,7 @@ class Solution:
         while mc-1-_d(mc-1) > ma:
             mb = (ma+mc)//2
             mb -= _d(mb)
-            if _c(mb) > k-1:
+            if _c(mb) >= k:
                 mc = mb
             else:
                 if ma == mb:
@@ -29,3 +31,23 @@ class Solution:
             if _c(dd)<k:
                 return d
             d = dd
+'''
+# 实际上，看答案后发现，由于寻找的是满足条件的下限，所以必然会找到表中的点，代码可以简化为：
+class Solution:
+    def findKthNumber(self, m: int, n: int, k: int) -> int:
+        def _c(x):
+            return sum(min(x//i,m) for i in range(1,min(x,n)+1))
+        
+       
+        if k == 1:
+            return 1
+        if k == m*n:
+            return m*n
+        ma,mc = 1,m * n
+        while mc > ma:
+            mb = (ma+mc)//2
+            if _c(mb) >= k:
+                mc = mb
+            else:
+                ma = mb+1
+        return ma
