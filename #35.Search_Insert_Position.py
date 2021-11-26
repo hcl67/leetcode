54825bc876fb9f1ff3a793fa22b04039e8853f18
hcl67 <djinn.luu@gmail.com>
2021-11-26 10:04:54 +0000

    Create #35.Search_Insert_Position.py

---
+++ #35.Search_Insert_Position.py
@@ -0,0 +1,7 @@
+'''
+https://leetcode.com/problems/search-insert-position/
+'''
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        return bisect.bisect_left(nums,target)
+        
