7ff8c2bd5f91002e619855c7b64e87ed5ecd0db1
hcl67 <djinn.luu@gmail.com>
2021-12-21 11:38:19 +0000

    Create #231.Power_of_Two.py

---
+++ #231.Power_of_Two.py
@@ -0,0 +1,13 @@
+'''
+https://leetcode.com/problems/power-of-two/
+'''
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n>1:
+            if n&1:
+                return False
+            n >>= 1
+        return True
+        
