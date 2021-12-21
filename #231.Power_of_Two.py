81e94b4e96b5c92099ca252e25bfa8fa6e331fe1
hcl67 <djinn.luu@gmail.com>
2021-12-21 12:03:32 +0000

    Update #231.Power_of_Two.py

--- #231.Power_of_Two.py
+++ #231.Power_of_Two.py
@@ -5,9 +5,12 @@ class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         if n <= 0:
             return False
+        '''
         while n>1:
             if n&1:
                 return False
             n >>= 1
         return True
+        '''
+        return (((n-1)^n) + 1) == (n<<1)
         
