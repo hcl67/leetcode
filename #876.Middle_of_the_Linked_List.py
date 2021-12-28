19528a3e4cd30895145998768cb7181c833835db
hcl67 <djinn.luu@gmail.com>
2021-12-28 11:09:56 +0000

    Create #876.Middle_of_the_Linked_List.py

---
+++ #876.Middle_of_the_Linked_List.py
@@ -0,0 +1,16 @@
+'''
+https://leetcode.com/problems/middle-of-the-linked-list/
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        lst,cur =[], head
+        while cur:
+            lst.append(cur)
+            cur = cur.next
+        return lst[len(lst)//2]
+        
